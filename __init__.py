1462ca4ef50cb8d727eb5381081e29ade69fd9a1
chenhaicangwu <166780401+chenhaicangwu@users.noreply.github.com>
2025-07-02 17:08:19 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,8 @@
 from .load_file_node import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+import os
+
+# 注册web目录
+WEB_DIRECTORY = os.path.join(os.path.dirname(os.path.realpath(__file__)), "web")
 
 # 导入API以注册路由
 try:
